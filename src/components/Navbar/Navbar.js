10ade6b3daad938e5435b4a79f13efe271044737
azal <azam.hosseynian@yahoo.com>
2024-02-21 22:58:11 +0330

    fix issue linked icon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,24 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Tooltip from '@mui/material/Tooltip';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import MenuIcon from '@mui/icons-material/Menu';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import TwitterIcon from '../../Icons/twitter.svg';
-import DiscordIcon from '../../Icons/discord.svg';
-import MediumIcon from '../../Icons/medium.svg'; 
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Tooltip from "@mui/material/Tooltip";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MenuIcon from "@mui/icons-material/Menu";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import TwitterIcon from "../../Icons/twitter.svg";
+import DiscordIcon from "../../Icons/discord.svg";
+import MediumIcon from "../../Icons/medium.svg";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const isMobile = useMediaQuery(theme => theme.breakpoints.down('md'));
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("md"));
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -29,39 +29,64 @@ function Navbar() {
   };
 
   const socialMediaIcons = [
-    { icon:  <img src={DiscordIcon} alt="Discord" width={24} height={24}/>, name: 'Discord', link:"https://discord.gg/EwEXrN7N" },
-    { icon: <img src={MediumIcon} alt="Medium" width={24} height={24}/>, name: 'Medium',link:"https://medium.com/@DeRand_dev" },
-    { icon: <img src={TwitterIcon} alt="Twitter" 
-              style={{ 
-                width: 24, 
-                height: 24, 
-                backgroundColor: isMobile ? 'black' : 'transparent',
-                borderRadius:'20px',
-              }} 
-      />, name: 'Twitter',link: "https://twitter.com/DeRand_dev"},
-    
-    { icon: <GitHubIcon style={{ width: 24, height: 24 }} />,  name: 'GitHub',link: "https://github.com/derand-protocol/"},
+    {
+      icon: <img src={DiscordIcon} alt="Discord" width={24} height={24} />,
+      name: "Discord",
+      link: "https://discord.gg/EwEXrN7N",
+    },
+    {
+      icon: <img src={MediumIcon} alt="Medium" width={24} height={24} />,
+      name: "Medium",
+      link: "https://medium.com/@DeRand_dev",
+    },
+    {
+      icon: (
+        <img
+          src={TwitterIcon}
+          alt="Twitter"
+          style={{
+            width: 24,
+            height: 24,
+            backgroundColor: isMobile ? "black" : "transparent",
+            borderRadius: "20px",
+          }}
+        />
+      ),
+      name: "Twitter",
+      link: "https://twitter.com/DeRand_dev",
+    },
+
+    {
+      icon: <GitHubIcon style={{ width: 24, height: 24 }} />,
+      name: "GitHub",
+      link: "https://github.com/derand-protocol/",
+    },
   ];
 
   return (
-    <AppBar position="fixed" sx={{ 
-      background: 'rgba(28, 27, 35, .98)', 
-      boxShadow: 'none', 
-      left: 0, 
-      right: 0, 
-      width: '100vw', 
-      overflowX: 'hidden',
-      zIndex:26,
-    }}>
+    <AppBar
+      position="fixed"
+      sx={{
+        background: "rgba(28, 27, 35, .98)",
+        boxShadow: "none",
+        left: 0,
+        right: 0,
+        width: "100vw",
+        overflowX: "hidden",
+        zIndex: 26,
+      }}
+    >
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-        <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
-          <Link to="/">
-            <img src="/logo.png" alt="DeRand Logo" style={{ height: '40px', paddingLeft: '30px', padding: '20px' }} />
-          </Link>
-        </Box>
-
-
+          <Box sx={{ flexGrow: 1, display: "flex", alignItems: "center" }}>
+            <Link to="/">
+              <img
+                src="/logo.png"
+                alt="DeRand Logo"
+                style={{ height: "40px", paddingLeft: "30px", padding: "20px" }}
+              />
+            </Link>
+          </Box>
           {isMobile ? (
             <>
               <IconButton color="inherit" onClick={handleOpenNavMenu}>
@@ -69,28 +94,44 @@ function Navbar() {
               </IconButton>
               <Menu
                 anchorEl={anchorElNav}
-                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                anchorOrigin={{ vertical: "top", horizontal: "right" }}
                 keepMounted
-                transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                transformOrigin={{ vertical: "top", horizontal: "right" }}
                 open={Boolean(anchorElNav)}
                 onClose={handleCloseNavMenu}
               >
                 {socialMediaIcons.map((item, index) => (
                   <MenuItem key={index} onClick={handleCloseNavMenu}>
-                    <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                    <a
+                      href={item.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: 1,
+                        color: "inherit",
+                        textDecoration: "none",
+                      }}
+                    >
                       {item.icon}
                       <Typography>{item.name}</Typography>
-                    </Box>
+                    </a>
                   </MenuItem>
                 ))}
               </Menu>
             </>
           ) : (
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
               {socialMediaIcons.map((item, index) => (
                 <Tooltip title={item.name} key={index}>
                   {item.link ? (
-                    <a href={item.link} target="_blank" rel="noopener noreferrer" style={{ color: 'inherit', textDecoration: 'none' }}>
+                    <a
+                      href={item.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      style={{ color: "inherit", textDecoration: "none" }}
+                    >
                       <IconButton color="inherit" sx={{ mx: 1 }}>
                         {item.icon}
                       </IconButton>
